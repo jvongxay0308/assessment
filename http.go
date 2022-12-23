79e6d9f9b0a3b4f6e94c14d84e15ddfdb2ca452b
Jalana Vongxay <88074695+jvongxay0308@users.noreply.github.com>
2022-12-23 22:31:21 +0700

    Exp03 (#5)
    * Implemented update expense service
    
    * Added integration test update expense service
    
    * Implemented update expense handler
    
    * Register update expense handler

--- http.go
+++ http.go
@@ -19,6 +19,7 @@ func NewHandler(db *DB) *Handler {
 func (h *Handler) Install(e *echo.Echo) {
 	e.POST("/expenses", h.Create)
 	e.GET("/expenses/:id", h.Get)
+	e.PUT("/expenses/:id", h.Update)
 }
 
 func (h *Handler) Create(c echo.Context) error {
@@ -79,6 +80,54 @@ func (h *Handler) Get(c echo.Context) error {
 	case errors.Is(err, ErrClosed):
 		return echo.NewHTTPError(http.StatusServiceUnavailable, echo.Map{
 			"code":    http.StatusServiceUnavailable,
+			"message": err.Error(),
+		})
+
+	case errors.Is(err, ErrNoExpense):
+		return echo.NewHTTPError(http.StatusNotFound, echo.Map{
+			"code":    http.StatusNotFound,
+			"message": err.Error(),
+		})
+
+	default:
+		return echo.NewHTTPError(http.StatusInternalServerError, echo.Map{
+			"code":    http.StatusInternalServerError,
+			"message": "Internal Server Error",
+		})
+	}
+}
+
+func (h *Handler) Update(c echo.Context) error {
+	expense := &Expense{}
+	if err := c.Bind(expense); err != nil {
+		return err
+	}
+
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
+			"code":    http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+	expense.ID = id
+
+	ctx := c.Request().Context()
+	expense, err = h.db.Update(ctx, expense)
+
+	switch {
+	case err == nil:
+		return c.JSON(http.StatusOK, expense)
+
+	case errors.Is(err, ErrClosed):
+		return echo.NewHTTPError(http.StatusServiceUnavailable, echo.Map{
+			"code":    http.StatusServiceUnavailable,
+			"message": err.Error(),
+		})
+
+	case errors.Is(err, ErrInvalidExpense):
+		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
+			"code":    http.StatusBadRequest,
 			"message": err.Error(),
 		})
 
