aba9d754b4cec37e3dc81907958d96c69df5e37a
Jalana Vongxay <jvongxay0308@gmail.com>
2022-12-23 22:29:27 +0700

    Implemented update expense handler

--- http.go
+++ http.go
@@ -95,3 +95,51 @@ func (h *Handler) Get(c echo.Context) error {
 		})
 	}
 }
+
+func (h *Handler) Update(c echo.Context) error {
+	expense := &Expense{}
+	if err := c.Bind(expense); err != nil {
+		return err
+	}
+
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
+			"code":    http.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+	expense.ID = id
+
+	ctx := c.Request().Context()
+	expense, err = h.db.Update(ctx, expense)
+
+	switch {
+	case err == nil:
+		return c.JSON(http.StatusOK, expense)
+
+	case errors.Is(err, ErrClosed):
+		return echo.NewHTTPError(http.StatusServiceUnavailable, echo.Map{
+			"code":    http.StatusServiceUnavailable,
+			"message": err.Error(),
+		})
+
+	case errors.Is(err, ErrInvalidExpense):
+		return echo.NewHTTPError(http.StatusBadRequest, echo.Map{
+			"code":    http.StatusBadRequest,
+			"message": err.Error(),
+		})
+
+	case errors.Is(err, ErrNoExpense):
+		return echo.NewHTTPError(http.StatusNotFound, echo.Map{
+			"code":    http.StatusNotFound,
+			"message": err.Error(),
+		})
+
+	default:
+		return echo.NewHTTPError(http.StatusInternalServerError, echo.Map{
+			"code":    http.StatusInternalServerError,
+			"message": "Internal Server Error",
+		})
+	}
+}
